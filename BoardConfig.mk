002af2f20f1d8cea56dd99e7d2210cc423c8b609
RainSnow <2652609017@qq.com>
2018-02-10 12:32:16 +0800

    kiwi: Include legacy QCOM sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,6 +151,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/huawei/kiwi/sepolicy
