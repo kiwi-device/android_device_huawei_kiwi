33da8a91be12eaa8a8d75fa277b2861ea57c401d
RainSnow <2652609017@qq.com>
2018-02-11 06:18:30 +0000

    kiwi: Build DRM HAL

--- device.mk
+++ device.mk
@@ -225,6 +225,10 @@ PRODUCT_PACKAGES += \
     sensors.msm8916 \
     android.hardware.sensors@1.0-impl
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
